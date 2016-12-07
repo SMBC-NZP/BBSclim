240c22eb0034da184a2908b3eaa80a936562b79a
Clark <crushing05@gmail.com>
2016-12-07 13:41:34 -0500

    Add # stops as option in write_pao so known for simulation

--- R/write_pao.R
+++ R/write_pao.R
@@ -7,51 +7,58 @@
 #' @return A .pao file containing the detection histories, covariates, and summary information to input into Presence
 #' @export
 
-write_pao <- function(counts, clim, alpha, sim = FALSE){
-
-  tot_stops <- length(grep("count", names(counts))) # Stops/route
-  n_seasons <- max(counts$Year) - min(counts$Year) + 1
+write_pao <- function(counts, covs, alpha, sim = FALSE, TenStops){
   common <- code_lookup$common[code_lookup$alpha == toupper(alpha)]
-
-  ### Covert count data to long format
-  counts <- dplyr::select(counts, routeID, Year, grep("count", names(counts)))
-  counts <- tidyr::gather(counts, key = "stop", value = "n", -routeID, -Year)
-
-
-  ### Add column with presence/absence data
-  pres <- dplyr::mutate(counts, occ = ifelse(n > 0, 1, 0))
-  pres <- dplyr::select(pres, -n)
-
-  ### Covert back to wide w/ 1 column for each year/stop (i.e., svy)
-  pres <- tidyr::unite(pres, svy, Year, stop, sep = "_")
-  pres <- pres[!duplicated(pres),]
-  pres <- tidyr::spread(pres, key = svy, value = occ)
-  pres <- dplyr::arrange(pres, routeID)
-
-  det_hist <- dplyr::select(pres, -routeID)
-
-  spp_clim <- dplyr::arrange(spp_clim, routeID)
-  spp_clim2 <- dplyr::rename(spp_clim, Lat = Latitude, Lon = Longitude)
-  spp_clim2 <- dplyr::mutate(spp_clim2, Lat = (Lat - mean(Lat))/sd(Lat),
-                                        Lon = (Lon - mean(Lon))/sd(Lon))
-  spp_clim3 <- dplyr::mutate(spp_clim2, sq_Lat = Lat ^ 2, sq_Lon = Lon ^ 2)
-
-  for(ss in 1:tot_stops) {
-    sc <- scale(1:tot_stops)[ss];        #names(sc)  <- paste0('Stop',ss)
-    sc2 <- (scale(1:tot_stops)[ss])^2;   #names(sc2) <- paste0('sqStop',ss)
-    spp_clim3   <- cbind(spp_clim3, sc, sc2)
-    colnames(spp_clim3)[ncol(spp_clim3)-1] <- paste0('Stop',ss)
-    colnames(spp_clim3)[ncol(spp_clim3)] <- paste0('sq_Stop',ss)
-  }
-
-  sitecovs	<- dplyr::select(spp_clim3, -routeID)
+  if(TenStops) {tot_stops <- 5}else{tot_stops <- 50}
 
   if(!sim){
+    n_seasons <- max(counts$Year) - min(counts$Year) + 1
+
+
+    ### Covert count data to long format
+    counts <- dplyr::select(counts, routeID, Year, grep("count", names(counts)))
+    counts <- tidyr::gather(counts, key = "stop", value = "n", -routeID, -Year)
+
+
+    ### Add column with presence/absence data
+    pres <- dplyr::mutate(counts, occ = ifelse(n > 0, 1, 0))
+    pres <- dplyr::select(pres, -n)
+
+    ### Covert back to wide w/ 1 column for each year/stop (i.e., svy)
+    pres <- tidyr::unite(pres, svy, Year, stop, sep = "_")
+    pres <- pres[!duplicated(pres),]
+    pres <- tidyr::spread(pres, key = svy, value = occ)
+    pres <- dplyr::arrange(pres, routeID)
+
+    det_hist <- dplyr::select(pres, -routeID)
+
+    spp_clim <- dplyr::arrange(covs, routeID)
+    spp_clim2 <- dplyr::rename(spp_clim, Lat = Latitude, Lon = Longitude)
+    spp_clim2 <- dplyr::mutate(spp_clim2, Lat = (Lat - mean(Lat))/sd(Lat),
+                                        Lon = (Lon - mean(Lon))/sd(Lon))
+    spp_clim3 <- dplyr::mutate(spp_clim2, sq_Lat = Lat ^ 2, sq_Lon = Lon ^ 2)
+
+    for(ss in 1:tot_stops) {
+      sc <- scale(1:tot_stops)[ss];        #names(sc)  <- paste0('Stop',ss)
+      sc2 <- (scale(1:tot_stops)[ss])^2;   #names(sc2) <- paste0('sqStop',ss)
+      spp_clim3   <- cbind(spp_clim3, sc, sc2)
+      colnames(spp_clim3)[ncol(spp_clim3)-1] <- paste0('Stop',ss)
+      colnames(spp_clim3)[ncol(spp_clim3)] <- paste0('sq_Stop',ss)
+    }
+
+    sitecovs	<- dplyr::select(spp_clim3, -routeID)
+
     spp_pao <- RPresence::create.pao(data = det_hist, nsurveyseason = rep(tot_stops, n_seasons),
-                        unitcov = sitecovs, survcov = NULL,
-                        title = paste(common, "PRESENCE Analysis", sep = " "),
-                        paoname = paste("inst/output/pao", paste(alpha, ".pao", sep = ""), sep = "/"))
+                                     unitcov = sitecovs, survcov = NULL,
+                                     title = paste(common, "PRESENCE Analysis", sep = " "),
+                                     paoname = paste("inst/output/pao", paste(alpha, ".pao", sep = ""), sep = "/"))
+
   }else{
+    det_hist <- counts
+    sitecovs <- covs
+
+    n_seasons <- dim(counts)[2] / tot_stops
+
     spp_pao <- RPresence::create.pao(data = det_hist, nsurveyseason = rep(tot_stops, n_seasons),
                                      unitcov = sitecovs, survcov = NULL,
                                      title = paste(common, "PRESENCE Analysis", sep = " "),
