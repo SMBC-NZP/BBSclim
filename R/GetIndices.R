fc95bd4a9dc4f2ee866ba24caca5bcf68c8a341f
Clark Rushing <crushing05@gmail.com>
2017-01-25 10:28:48 -0500

    Fix number of sites in GetIndices

--- R/GetIndices.R
+++ R/GetIndices.R
@@ -20,31 +20,31 @@ GetIndices <- function(alpha){
   prob_grp <- dplyr::group_by(prob_df, Year)
 
   avg.psi <- dplyr::summarise(prob_grp, value = mean(Prob))
-  avg.psi$sd.err <- delta(index = "avg.psi", est = avg.psi$value, alpha, nSites = dim(climate)[1], years, buff = spp_buff)
+  avg.psi$sd.err <- delta(index = "avg.psi", est = avg.psi$value, alpha, nSites = dim(prob_df)[1], years, buff = spp_buff)
   avg.psi$ind <- "avg.psi"
 
   s.lat <- dplyr::summarise(prob_grp, value = range.limit(cell.probs = Prob, prob = 0.99, coord = lat, limit = "south"))
-  s.lat$sd.err <- delta(index = "s.lat", est = s.lat$value, alpha, nSites = dim(climate)[1], years, buff = spp_buff)
+  s.lat$sd.err <- delta(index = "s.lat", est = s.lat$value, alpha, nSites = dim(prob_df)[1], years, buff = spp_buff)
   s.lat$ind <- "s.lat"
 
   s.core <- dplyr::summarise(prob_grp, value = range.limit(cell.probs = Prob, prob = 0.75, coord = lat, limit = "south"))
-  s.core$sd.err <- delta(index = "s.core", est = s.core$value, alpha, nSites = dim(climate)[1], years, buff = spp_buff)
+  s.core$sd.err <- delta(index = "s.core", est = s.core$value, alpha, nSites = dim(prob_df)[1], years, buff = spp_buff)
   s.core$ind <- "s.core"
 
   n.lat <- dplyr::summarise(prob_grp, value = range.limit(cell.probs = Prob, prob = 0.99, coord = lat, limit = "north"))
-  n.lat$sd.err <- delta(index = "n.lat", est = n.lat$value, alpha,nSites = dim(climate)[1], years, buff = spp_buff)
+  n.lat$sd.err <- delta(index = "n.lat", est = n.lat$value, alpha,nSites = dim(prob_df)[1], years, buff = spp_buff)
   n.lat$ind <- "n.lat"
 
   n.core <- dplyr::summarise(prob_grp, value = range.limit(cell.probs = Prob, prob = 0.75, coord = lat, limit = "north"))
-  n.core$sd.err <- delta(index = "n.core", est = n.core$value, alpha, nSites = dim(climate)[1], years, buff = spp_buff)
+  n.core$sd.err <- delta(index = "n.core", est = n.core$value, alpha, nSites = dim(prob_df)[1], years, buff = spp_buff)
   n.core$ind <- "n.core"
 
   avg.lat <- dplyr::summarise(prob_grp, value = sum(lat * Prob)/sum(Prob))
-  avg.lat$sd.err <- delta(index = "avg.lat", est = avg.lat$value, alpha, nSites = dim(climate)[1], years, buff = spp_buff)
+  avg.lat$sd.err <- delta(index = "avg.lat", est = avg.lat$value, alpha, nSites = dim(prob_df)[1], years, buff = spp_buff)
   avg.lat$ind <- "avg.lat"
 
   avg.lon <- dplyr::summarise(prob_grp, value = sum(lon * Prob)/sum(Prob))
-  avg.lon$sd.err <- delta(index = "avg.lon", est = avg.lon$value, alpha, nSites = dim(climate)[1], years, buff = spp_buff)
+  avg.lon$sd.err <- delta(index = "avg.lon", est = avg.lon$value, alpha, nSites = dim(prob_df)[1], years, buff = spp_buff)
   avg.lon$ind <- "avg.lon"
 
   indices <- dplyr::bind_rows(avg.psi, s.lat)
