f3a2ef329512141341257f3fb6bb2288b4c40524
Clark <crushing05@gmail.com>
2017-03-30 15:05:37 -0400

    Fix call to config

--- run_BBSclim.R
+++ run_BBSclim.R
@@ -1,11 +1,11 @@
 
-httr::set_config(config(ssl_verifypeer = 0L))
+httr::set_config(httr::config(ssl_verifypeer = 0L))
 
 ### Install BBSclim package
 install.packages("devtools")
 devtools::install_github('SMBC-NZP/BBSclim')
 
-
+### Run analysis
 BBSclim::run_BBSclim()
 
 
