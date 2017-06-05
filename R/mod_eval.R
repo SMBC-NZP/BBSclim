fe72cabcc2aba72942feadf93debc494d11dabd5
Clark <crushing05@gmail.com>
2017-06-05 11:07:57 -0400

    Add sign check for lat2/lon2 in psi model

--- R/mod_eval.R
+++ R/mod_eval.R
@@ -33,9 +33,9 @@ mod_eval <- function(pres_out, mod, pao2, psi = FALSE, dig = 2, large = 8, is.an
       quads <- grep("psi.sq", betas)
       quads.coord <- quads[(length(quads) - 1):length(quads)]
       quads.clim <- quads[!(quads %in% quads.coord)]
-      psi.check[quads.coord - 1] <- coefs[quads.coord]/std.er[quads.coord] < 0
+      psi.check[quads.coord - 1] <- coefs[quads.coord] < 0
       if(strict.psi){
-        psi.check[quads.clim - 1] <- coefs[quads.clim]/std.er[quads.clim] < 0
+        psi.check[quads.clim - 1] <- coefs[quads.clim] < 0
       }else{
         psi.check[quads.clim - 1] <- coefs[quads.clim]/std.er[quads.clim] < 1.96
       }
@@ -118,27 +118,33 @@ mod_eval <- function(pres_out, mod, pao2, psi = FALSE, dig = 2, large = 8, is.an
             quads <- grep("psi.sq", betas)
             quads.coord <- quads[(length(quads) - 1):length(quads)]
             quads.clim <- quads[!(quads %in% quads.coord)]
-            psi.check[quads.coord - 1] <- coefs[quads.coord]/std.er[quads.coord] < 0
+            psi.check[quads.coord - 1] <- coefs[quads.coord] < 0
             if(strict.psi){
-              psi.check[quads.clim - 1] <- coefs[quads.clim]/std.er[quads.clim] < 0
+              psi.check[quads.clim - 1] <- coefs[quads.clim] < 0
             }else{
               psi.check[quads.clim - 1] <- coefs[quads.clim]/std.er[quads.clim] < 1.96
             }
           }
           if(num.betas[4] > 1) {
             quads <- grep("gam1.sq", betas)
+            quads.coord <- quads[(length(quads) - 1):length(quads)]
+            quads.clim <- quads[!(quads %in% quads.coord)]
+            gam.check[quads.coord - 4 - sum(num.betas[1:3])] <- coefs[quads.coord] < 0
             if(strict.gam){
-              gam.check[quads - 4 - sum(num.betas[1:3])]	<- coefs[quads] < 0
+              gam.check[quads.clim - 4 - sum(num.betas[1:3])]	<- coefs[quads.clim] < 0
             }else{
-              gam.check[quads - 4 - sum(num.betas[1:3])]	<- coefs[quads] < 1.96
+              gam.check[quads.clim - 4 - sum(num.betas[1:3])]	<- coefs[quads.clim]/std.er[quads.clim] < 1.96
             }
           }
           if(num.betas[5] > 1) {
-            quads <- grep("eps1.sq",betas)
+            quads <- grep("eps1.sq", betas)
+            quads.coord <- quads[(length(quads) - 1):length(quads)]
+            quads.clim <- quads[!(quads %in% quads.coord)]
+            eps.check[quads.coord - 5 - sum(num.betas[1:4])] <- coefs[quads.coord] > 0
             if(strict.gam){
-              eps.check[quads - 5 - sum(num.betas[1:4])]	<- coefs[quads] > 0
+              eps.check[quads.clim - 5 - sum(num.betas[1:4])]	<- coefs[quads.clim] > 0
             }else{
-              eps.check[quads - 5 - sum(num.betas[1:4])]	<- coefs[quads] > 1.96
+              eps.check[quads.clim - 5 - sum(num.betas[1:4])]	<- coefs[quads.clim]/std.er[quads.clim] > 1.96
             }
           }
         }
