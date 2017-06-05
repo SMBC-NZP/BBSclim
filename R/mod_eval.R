41d0756ca7ec35f47685d88423b1f3472290f31c
Clark <crushing05@gmail.com>
2017-06-05 11:07:31 -0400

    Add sign check for lat2/lon2 in psi model

--- R/mod_eval.R
+++ R/mod_eval.R
@@ -26,13 +26,18 @@ mod_eval <- function(pres_out, mod, pao2, psi = FALSE, dig = 2, large = 8, is.an
       ## Check for negative SE
       psi.check 	<- !is.na(std.er[2:(num.betas[1] + 1)])
 
-      ## Check for very large SE
-      psi.check 	<- abs(std.er[2:(num.betas[1] + 1)]) < large
+      ## Check for very coeficients
+      psi.check 	<- abs(coefs[2:(num.betas[1] + 1)]) < large
 
       ## Check for significant wrong sign on quadratic terms
       quads <- grep("psi.sq", betas)
+      quads.coord <- quads[(length(quads) - 1):length(quads)]
+      quads.clim <- quads[!(quads %in% quads.coord)]
+      psi.check[quads.coord - 1] <- coefs[quads.coord]/std.er[quads.coord] < 0
       if(strict.psi){
-        psi.check[quads - 1] <- coefs[quads]/std.er[quads] < 1.96
+        psi.check[quads.clim - 1] <- coefs[quads.clim]/std.er[quads.clim] < 0
+      }else{
+        psi.check[quads.clim - 1] <- coefs[quads.clim]/std.er[quads.clim] < 1.96
       }
     }
 
@@ -81,25 +86,25 @@ mod_eval <- function(pres_out, mod, pao2, psi = FALSE, dig = 2, large = 8, is.an
     if(mean(c(psi.check, th0.check, th1.check, gam.check, eps.check, p1.check, p2.check, 1))==1) {
       if(nuisance == FALSE){
         if(num.betas[1]>1) {
-          psi.check 	<- abs(std.er[2:(num.betas[1] + 1)]) < large
+          psi.check 	<- abs(coefs[2:(num.betas[1] + 1)]) < large
         }
         if(num.betas[4]>1) {
-          gam.check 	<- abs(std.er[(5 + sum(num.betas[1:3])):(sum(num.betas[1:4]) + 4)]) < large
+          gam.check 	<- abs(coefs[(5 + sum(num.betas[1:3])):(sum(num.betas[1:4]) + 4)]) < large
         }
         if(num.betas[5]>1) {
-          eps.check <- abs(std.er[(6 + sum(num.betas[1:4])):(sum(num.betas[1:5]) + 5)]) < large
+          eps.check <- abs(coefs[(6 + sum(num.betas[1:4])):(sum(num.betas[1:5]) + 5)]) < large
         }
       }
 
       if(num.betas[2]>1) {
-        th0.check 	<- abs(std.er[(3 + num.betas[1]):(sum(num.betas[1:2]) + 2)]) < large
+        th0.check 	<- abs(coefs[(3 + num.betas[1]):(sum(num.betas[1:2]) + 2)]) < large
       }
       if(num.betas[3]>1) {
-        th1.check 	<- abs(std.er[(4 + sum(num.betas[1:2])):(sum(num.betas[1:3]) + 3)]) < large
+        th1.check 	<- abs(coefs[(4 + sum(num.betas[1:2])):(sum(num.betas[1:3]) + 3)]) < large
       }
       if(num.betas[6]>1) {
-        p1.check <- p2.check <- abs(std.er[(7 + sum(num.betas[1:5])):(sum(num.betas[1:6]) + 5)]) < large
-        if(is.het)	p2.check   	<- abs(std.er[(7 + sum(num.betas[1:6])):(sum(num.betas[1:6]) + 5 + num.betas[6])])<large
+        p1.check <- p2.check <- abs(coefs[(7 + sum(num.betas[1:5])):(sum(num.betas[1:6]) + 5)]) < large
+        if(is.het)	p2.check <- abs(coefs[(7 + sum(num.betas[1:6])):(sum(num.betas[1:6]) + 5 + num.betas[6])])<large
       }
     }	# end the if statement
 
@@ -111,20 +116,29 @@ mod_eval <- function(pres_out, mod, pao2, psi = FALSE, dig = 2, large = 8, is.an
         if(mean(c(psi.check, th0.check, th1.check, gam.check, eps.check, p1.check, p2.check, 1))==1) {
           if(num.betas[1] > 1) {
             quads <- grep("psi.sq", betas)
+            quads.coord <- quads[(length(quads) - 1):length(quads)]
+            quads.clim <- quads[!(quads %in% quads.coord)]
+            psi.check[quads.coord - 1] <- coefs[quads.coord]/std.er[quads.coord] < 0
             if(strict.psi){
-              psi.check[quads - 1]	<- coefs[quads] < 0
+              psi.check[quads.clim - 1] <- coefs[quads.clim]/std.er[quads.clim] < 0
+            }else{
+              psi.check[quads.clim - 1] <- coefs[quads.clim]/std.er[quads.clim] < 1.96
             }
           }
-          if(num.betas[4]>1) {
+          if(num.betas[4] > 1) {
             quads <- grep("gam1.sq", betas)
             if(strict.gam){
               gam.check[quads - 4 - sum(num.betas[1:3])]	<- coefs[quads] < 0
+            }else{
+              gam.check[quads - 4 - sum(num.betas[1:3])]	<- coefs[quads] < 1.96
             }
           }
-          if(num.betas[5]>1) {
+          if(num.betas[5] > 1) {
             quads <- grep("eps1.sq",betas)
             if(strict.gam){
               eps.check[quads - 5 - sum(num.betas[1:4])]	<- coefs[quads] > 0
+            }else{
+              eps.check[quads - 5 - sum(num.betas[1:4])]	<- coefs[quads] > 1.96
             }
           }
         }
